95de2df05fb6e4c79079ae9532c7dee7fcbf7ad7
afgomezv <afgomezvonline@gmail.com>
2024-11-17 11:46:17 -0500

    high-order-component

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,27 @@
+import { ComponentType } from "react";
+
+function withLoading<T extends object>(Component: ComponentType<T>) {
+  return (props: T & { isLoading: boolean }) => {
+    const { isLoading, ...rest } = props;
+    return isLoading ? <p>Loading...</p> : <Component {...(rest as T)} />;
+  };
+}
+
+type UserProps = {
+  name: string;
+};
+
+const UserComponent = ({ name }: UserProps) => {
+  return <div>Hello, {name}!</div>;
+};
+
+const UserWithLoading = withLoading(UserComponent);
+
 export const ParentComponent = () => {
   return (
     <div>
+      <UserWithLoading isLoading={true} name="Andrey" />
+      <UserWithLoading isLoading={false} name="Andrey" />
     </div>
   );
 };
